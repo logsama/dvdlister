aaad559cb513f715fd2bde2aebe37082e18bc720
Chris Logsdon <logsama@gmail.com>
2016-04-06 15:07:12 -0700

    sort titles alpha

--- routes/dvd.js
+++ routes/dvd.js
@@ -4,14 +4,21 @@ var mongoose = require('mongoose');
 var Dvd = mongoose.model('dvds');
 
 //Get Dvds Listing
-exports.index  = function(req,res){
+exports.index  = function(req,res) {
+/*
     Dvd.find(function(err, Dvd) {
         if (err) return res.render('Error occurred');
         res.send(Dvd);
     });
+*/
+    Dvd.find({},null,sort: {title: 1}}, function (err, titles) {
+            if (err) return res.render('Error occurred');
+            res.send(titles);
+        });
+    )
 };
 
-exports.findById = function(req,res){
+exports.findById = function(req,res) {
     Dvd.findById( req.params.id, function( err, Dvd ) {
             if (err) {
                 res.send('Error occurred');
@@ -21,22 +28,22 @@ exports.findById = function(req,res){
     });
 };
 
-exports.newDvd = function(req,res){
-    var emp = new Dvd(req.body);
+exports.newDvd = function(req,res) {
+    var aDVD = new Dvd(req.body);
 
-    console.log(emp.title);
+    console.log(aDVD.title);
 
     emp.save(function(err) {
         if (err) {
             res.send('Error occurred');
             return console.log(err);
         }
-        res.send(emp);
+        res.send(aDVD);
     });
 }
 
-exports.update = function(req,res){
-    Dvd.findById( req.params.id, function( err, Dvd ) {
+exports.update = function(req,res) {
+    Dvd.findById( req.params.id, function(err,Dvd) {
         if (!Dvd) {
             res.send('Dvd not found with given id');
         }
@@ -47,7 +54,7 @@ exports.update = function(req,res){
             Dvd.set(req.body)
             if (Dvd.isModified()) {
                 Dvd.increment();
-                Dvd.save(function(err){
+                Dvd.save(function(err) {
                     if (err) {
                         res.send('Error occurred');
                         return console.log(err);
@@ -64,7 +71,7 @@ exports.update = function(req,res){
 };
 
 exports.delete = function(req,res) {
-    Dvd.findById( req.params.id, function( err, Dvd ) {
+    Dvd.findById( req.params.id, function(err,Dvd) {
         if (!Dvd){
             return res.send('Dvd not found with given id');
         }
